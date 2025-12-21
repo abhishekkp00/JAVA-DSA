cbb5ee1645c3d21397e1db4a611125c8e64b7ee5
abhishekkp00 <abhishekforcollege@gmail.com>
2025-12-21 07:49:25 +0530

    Basics of input in java adding comment

--- Input.java
+++ Input.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age = Integer.parseInt(sc.nextLine());
+        int age = Integer.parseInt(sc.nextLine()); //parseInt is used when we intended to take both string and int as input
         String name = sc.nextLine();
         System.out.println("Hello "+ name+ " your age is " +age+ " years.");
 
